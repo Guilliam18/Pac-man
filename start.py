4dfab541a09189c8667860080d992505b800117a
Guilliam18 <Guilliams18@gmail.com>
2022-08-08 21:34:47 -0400

    Title & Icon

--- start.py
+++ start.py
@@ -1,8 +1,11 @@
 import tkinter as tk
+from tkinter import ttk
 
 def main():
     gui = tk.Tk()
-    gui.geometry('1200x800')
+    gui.title("Pac-Man")
+    gui.geometry('1200x800+400-200')
+    gui.iconbitmap('./Pictures/icon.ico')
     gui['bg']='black'
     gui.mainloop()
 
